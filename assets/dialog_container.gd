efea5b1c9489fa00056ecfbc08f00a780b469dc1
bogo8liuk <lucaborghi99@gmail.com>
2025-06-03 22:10:10 +0200

    feat: handle on press event

--- assets/dialog_container.gd
+++ assets/dialog_container.gd
@@ -7,8 +7,13 @@ func _ready() -> void:
 	$ChoicesContainer/ChoiceButton1.text = "sì"
 	$ChoicesContainer/ChoiceButton2.text = "no"
 	$ChoicesContainer/ChoiceButton3.text = "non lo so"
-
+	
+	$ChoicesContainer/ChoiceButton1.pressed.connect(on_press_event)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+	
+func on_press_event() -> void:
+	print("ciao mondo!")
 	pass
