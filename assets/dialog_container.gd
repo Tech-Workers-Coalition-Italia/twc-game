453c13b44ea5d5204db7fd9277062c440fe2af13
bogo8liuk <lucaborghi99@gmail.com>
2025-06-16 17:31:53 +0200

    feat: read json

--- assets/dialog_container.gd
+++ assets/dialog_container.gd
@@ -10,6 +10,12 @@ func _ready() -> void:
 	$ChoicesContainer/ChoiceButton3.text = "non lo so"
 	
 	$ChoicesContainer/ChoiceButton1.pressed.connect(on_press_event)
+	
+	#config.set_value("Player.Features.Stats", "hp", 100)
+	#config.save("res://assets/questions.cfg")
+	var questions = read_questions()
+	for k in questions.keys():
+		print(questions[k])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,3 +25,12 @@ func on_press_event() -> void:
 	print("hello world!")
 	button1_pressed.emit()
 	pass
+
+func read_questions() -> Dictionary:
+	var file = FileAccess.open("res://questions.json", FileAccess.READ)
+	var content = file.get_as_text()
+	
+	var json = JSON.new()
+	var _res = json.parse(content)
+	
+	return json.data
